207f5e5caac87f106947215cd790bb2978b71f4a
nguyentdung <nguyentdung@dena-travel.com>
2016-08-03 10:05:37 +0900

    update setup zsh file

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,9 +1,11 @@
 brew install zsh
 
-# install zsh
+# install zsh & zsh completions
 if [ ! -x "$(command -v zsh)" ]; then
   echo "[start install zsh]"
   brew install zsh
+  brew install zsh-completions
+  rm -f ~/.zcompdump
   echo "/usr/local/bin/zsh" |  sudo tee -a /etc/shells > /dev/null
   echo "[successfully installed] zsh"
 fi
